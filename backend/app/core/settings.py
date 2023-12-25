6953e1b9a84e0b41398ad73cf2de4a217f8d122f
Elayade Ismail <elayadeismail@gmail.com>
2023-12-25 17:11:44 +0100

    Add env variables to settings

--- backend/app/core/settings.py
+++ backend/app/core/settings.py
@@ -1,1 +1,15 @@
-print("Settings")
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from functools import lru_cache
+
+
+class Settings(BaseSettings):
+    app_name: str = "Issue Tracker"
+    DATABASE_URL: str
+    
+    model_config = SettingsConfigDict(env_file=".env")
+
+@lru_cache
+def get_settings():
+    return Settings()
+
+settings = get_settings()
