10448af975cf44cd585996e17d79e0368afeec7a
Elayade Ismail <elayadeismail@gmail.com>
2023-12-27 20:33:50 +0100

    change SQLALCHEMY_DATABASE_URI type to fix server crash

--- backend/app/core/settings.py
+++ backend/app/core/settings.py
@@ -2,14 +2,13 @@ import secrets
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from functools import lru_cache
 
-from pydantic import PostgresDsn
 
 
 class Settings(BaseSettings):
     SECRET_KEY: str = secrets.token_urlsafe(32)
     # 60 minutes * 24 hours * 8 days = 8 days
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
-    SQLALCHEMY_DATABASE_URI: PostgresDsn
+    SQLALCHEMY_DATABASE_URI: str
     
     model_config = SettingsConfigDict(env_file=".env")
 
