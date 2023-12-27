d5a05faa7b0b2331c6d45224355b5ea661545fab
Elayade Ismail <elayadeismail@gmail.com>
2023-12-27 20:34:37 +0100

    add userOut and FindUser Schemas

--- backend/app/schemas/users.py
+++ backend/app/schemas/users.py
@@ -5,3 +5,12 @@ class CreateUser(SQLModel):
     email:EmailStr;
     full_name:str = Field(min_length=3,max_length=250)
     password: str = Field(min_length=8,max_length=100)
+    
+class UserOut(SQLModel):
+    id:int;
+    email:str
+    full_name:str
+    
+class FindUser(SQLModel):
+    id:int | None;
+    email:EmailStr|None 
