f5f22d7dbf6bfda6efe5d5a9e8fbbd257de94932
Elayade Ismail <elayadeismail@gmail.com>
2023-12-27 20:34:47 +0100

    add register endpoint

--- backend/app/routers/auth.py
+++ backend/app/routers/auth.py
@@ -1,4 +1,6 @@
 from fastapi import APIRouter
+from app.schemas.users import UserOut,CreateUser
+from app.repositories.users import create_user
 
 
 router = APIRouter()
@@ -8,6 +10,9 @@ async def login():
     return {"message":"Login Endpoint"}
 
 
-@router.post("/register")
-async def register():
-    return {"message":"Login Endpoint"}
+@router.post("/register",status_code=201)
+async def register(user_input:CreateUser):
+    user = await create_user(user_input)
+    print(user)
+    userResponse = UserOut(email=user.email,id=user.id,full_name=user.full_name)
+    return {"user":userResponse}
