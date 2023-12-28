bad13d275ade74e561594e5f193a7905b6c6785d
Elayade Ismail <elayadeismail@gmail.com>
2023-12-28 19:35:04 +0100

    Add option to get users by username

--- backend/app/repositories/users.py
+++ backend/app/repositories/users.py
@@ -1,18 +1,22 @@
 from fastapi import HTTPException,status
 from app.database.database import engine
 from app.schemas.users import CreateUser,FindUser
-from sqlmodel import select,Session
+from sqlmodel import select,Session,or_
 from app.models.users import User
 from app.core.security import get_password_hash
 
 async def create_user(user_args:CreateUser):
     with Session(engine) as session:
-        find_with_email_statement = select(User).where(User.email == user_args.email)
-        user_with_email = session.exec(find_with_email_statement).first()
-        if user_with_email is not None:
-            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail="Email Already Exists")
+        existing_user_statement = select(User).where(or_(User.email == user_args.email,User.username == user_args.username))
+        existing_user = session.exec(existing_user_statement).first()
+        if existing_user:
+            if existing_user.username == user_args.username:
+                field = "username"
+            else:
+                field = "email"
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail= f"{field} already exists")
         hashed_password = get_password_hash(user_args.password)
-        user = User(email=user_args.email,full_name=user_args.full_name,hashed_password=hashed_password,is_active=True,is_superuser=False)
+        user = User(email=user_args.email,username=user_args.username,full_name=user_args.full_name,hashed_password=hashed_password,is_active=True,is_superuser=False)
         session.add(user)
         session.commit()
         session.refresh(user)
@@ -23,8 +27,10 @@ async def find_user(args:FindUser):
     with Session(engine) as session: 
         if args.id:
             statement = select(User).where(User.id == args.id) 
-        else:
+        elif args.email:
             statement = select(User).where(User.email == args.email) 
+        else :
+            statement = select(User).where(User.username == args.username)
         user = session.exec(statement).first()
         return user
 
--- backend/app/schemas/users.py
+++ backend/app/schemas/users.py
@@ -1,16 +1,21 @@
 from sqlmodel import SQLModel,Field
 from pydantic import EmailStr
+from typing import Optional
 
 class CreateUser(SQLModel):
     email:EmailStr;
+    username:str = Field(min_length=3,max_length=100);
     full_name:str = Field(min_length=3,max_length=250)
     password: str = Field(min_length=8,max_length=100)
     
 class UserOut(SQLModel):
     id:int;
+    username:str;
     email:str
     full_name:str
     
 class FindUser(SQLModel):
-    id:int | None;
-    email:EmailStr|None 
+    id: int | None = None;
+    username: str | None = None;
+    email:EmailStr|None = None 
+    
