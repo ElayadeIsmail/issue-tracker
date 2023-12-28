dca7aee2735126ab3bc5a047e6ab405feb3970a7
Elayade Ismail <elayadeismail@gmail.com>
2023-12-28 20:42:53 +0100

    Add Create Tables method

--- backend/app/database/database.py
+++ backend/app/database/database.py
@@ -1,4 +1,9 @@
-from sqlmodel import create_engine
+from sqlmodel import create_engine,SQLModel
 from app.core.settings import settings
 
 engine = create_engine(settings.SQLALCHEMY_DATABASE_URI)
+
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
