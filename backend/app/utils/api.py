e074b756fcbe8bb8017e9fd8241c8c42294b4659
Elayade Ismail <elayadeismail@gmail.com>
2023-12-30 18:57:53 +0100

    Add bad_request_error helpers

--- backend/app/utils/api.py
+++ backend/app/utils/api.py
@@ -3,3 +3,6 @@ from fastapi import HTTPException,status
 
 def not_found_error(message:Optional[str]="Resource Not Found"):
     return HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=message)
+
+def bad_request_error(message:str):
+    return HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail=message)
