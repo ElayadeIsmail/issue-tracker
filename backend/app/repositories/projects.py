622399e9d963065261e52173d6db9c6f8e6ef53c
Elayade Ismail <elayadeismail@gmail.com>
2023-12-30 15:10:29 +0100

    Add Get CurrentUser Projects Endpoint

--- backend/app/repositories/projects.py
+++ backend/app/repositories/projects.py
@@ -1,7 +1,8 @@
-from sqlmodel import Session
+from sqlmodel import Session,select
 from app.database.database import engine
 from app.models.projects import Project
 from app.models.users import User
+from app.models.projects_to_users import ProjectUserLink
 from app.schemas.projects import CreateProject
 
 
@@ -13,3 +14,11 @@ async def create_project(project_args:CreateProject,admin:User):
         
         session.refresh(project)
     return project
+
+
+async def get_current_user_projects(current_user:User):
+    with Session(engine) as session:
+        statement = select(Project).where(ProjectUserLink.user_id == current_user.id)
+        # Execute the query
+        projects = session.exec(statement).all()
+        return projects
--- backend/app/routers/projects.py
+++ backend/app/routers/projects.py
@@ -2,13 +2,18 @@ from typing import Annotated
 from fastapi import APIRouter, Depends,status
 from app.auth.auth import get_current_user
 from app.models.users import User
-from app.repositories.projects import create_project as db_create_project
+from app.repositories.projects import create_project as db_create_project,get_current_user_projects as db_get_current_user_projects
 
-from app.schemas.projects import CreateProject, ProjectOut
+from app.schemas.projects import CreateProject
 
 router = APIRouter()
 
-@router.post("/",status_code=status.HTTP_201_CREATED)
+@router.post("/",name="Create New Project",status_code=status.HTTP_201_CREATED)
 async def create_project(create_project_args:CreateProject,current_user: Annotated[User, Depends(get_current_user)]):
     project = await db_create_project(project_args=create_project_args,admin=current_user)
     return project
+
+@router.get("/",name="Get Current User Projects")
+async def get_current_user_projects(current_user: Annotated[User, Depends(get_current_user)]):
+    projects = await db_get_current_user_projects(current_user)
+    return projects
