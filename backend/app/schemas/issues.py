8422292fbf18addee3b03ffaeb51865c12ad1204
Elayade Ismail <elayadeismail@gmail.com>
2023-12-30 16:45:07 +0100

    Add Create Issue request Body model

---
+++ backend/app/schemas/issues.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+from typing import Optional
+from sqlmodel import SQLModel,Field
+
+from app.models.issues import IssuePriority, IssueStatus
+
+
+class CreateIssue(SQLModel):
+    name:str=Field(min_length=3)
+    description:Optional[str]=Field(default=None)
+    status:Optional[IssueStatus] = Field(default=IssueStatus.not_started)
+    priority:Optional[IssuePriority] =Field(default=None)
+    due_date: Optional[datetime] = Field(default=None)
+    assignee_id: Optional[int] = Field(default=None)
