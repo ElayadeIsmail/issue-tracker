2db9d0dc8c5599c1d5074fe20297c1f997eeae14
Elayade Ismail <elayadeismail@gmail.com>
2023-12-31 16:21:20 +0100

    Add Put And Delete Issue

--- backend/app/repositories/issues.py
+++ backend/app/repositories/issues.py
@@ -4,20 +4,20 @@ from app.models.projects import Project
 from app.models.projects_to_users import ProjectUserLink
 
 from app.models.users import User
-from app.schemas.issues import CreateIssue
+from app.schemas.issues import IssueIn
 from ..database.database import engine
-from app.utils.api import bad_request_error, not_found_error
+from app.utils.api import bad_request_error, not_found_error, unauthorized_request_error
 
 async def user_has_right_to_project(project_id:int,user_id:int):
     with Session(engine) as session:
         project_statement = select(Project).join(ProjectUserLink).where(Project.id == project_id,ProjectUserLink.user_id == user_id)
         project = session.exec(project_statement).first()
-        return project != None
+        return project
     
 
 async def get_project_issues(project_id:int,current_user:User):
-    has_access = await user_has_right_to_project(project_id,current_user.id)
-    if not has_access:
+    project = await user_has_right_to_project(project_id,current_user.id)
+    if not project:
         raise not_found_error()
     with Session(engine) as session:
         statement = select(Issue,User).outerjoin(User,Issue.assignee_id == User.id).where(Issue.project_id == project_id).order_by(Issue.updated_at.desc())
@@ -28,9 +28,9 @@ async def get_project_issues(project_id:int,current_user:User):
         return issues
 
 
-async def create_issue(project_id:int,issue_args:CreateIssue,current_user:User):
-    has_access = await user_has_right_to_project(project_id,current_user.id)
-    if not has_access:
+async def create_issue(project_id:int,issue_args:IssueIn,current_user:User):
+    project = await user_has_right_to_project(project_id,current_user.id)
+    if not project:
         raise not_found_error()
     if issue_args.assignee_id:
         assignee_access = await user_has_right_to_project(project_id, issue_args.assignee_id)
@@ -44,3 +44,39 @@ async def create_issue(project_id:int,issue_args:CreateIssue,current_user:User):
         # IDK why if I removed it it doesn't work
         issue.assignee = issue.assignee
         return issue
+    
+async def update_issue(project_id:int,issue_id:int,issue_args:IssueIn,current_user:User):
+    project = await user_has_right_to_project(project_id,current_user.id)
+    if not project:
+        raise not_found_error() 
+    with Session(engine) as session:
+        issue = session.exec(select(Issue).where(Issue.id == issue_id)).first() 
+        if not issue or issue.project_id != project_id:
+            raise not_found_error()
+        if issue_args.assignee_id and issue.assignee_id != issue_args.assignee_id:
+            project = await user_has_right_to_project(project_id,issue_args.assignee_id)
+            if not project:
+                raise bad_request_error("assignee doesn't have access to this project")
+            issue.name = issue_args.name
+            issue.description = issue_args.description
+            issue.due_date = issue_args.due_date
+            issue.status = issue_args.status
+            issue.priority = issue_args.priority
+            issue.assignee_id = issue_args.assignee_id
+            session.add(issue)
+            session.refresh(issue)
+            return issue
+    
+async def delete_issue(project_id:int,issue_id:int,current_user:User):
+    project = await user_has_right_to_project(project_id,current_user.id)
+    if not project:
+        raise not_found_error() 
+    if project.admin_id != current_user.id:
+        raise unauthorized_request_error("Only Project Admin Can Delete Issues")
+    with Session(engine) as session:
+        issue = session.exec(select(Issue).where(Issue.id == issue_id)).first() 
+        if not issue or issue.project_id != project_id:
+            raise not_found_error()
+        session.delete(issue)
+        session.commit()
+        return
--- backend/app/routers/issues.py
+++ backend/app/routers/issues.py
@@ -1,12 +1,12 @@
 
 
 from typing import Annotated,List
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends,status
 from app.auth.auth import get_current_user
-from app.repositories.issues import get_project_issues as db_get_project_issues,create_issue as db_create_issue
+from app.repositories.issues import get_project_issues as db_get_project_issues,create_issue as db_create_issue,update_issue as db_update_issue ,delete_issue as db_delete_issue
 
 from app.models.users import User
-from app.schemas.issues import CreateIssue, IssueOut
+from app.schemas.issues import IssueIn, IssueOut
 
 
 router = APIRouter()
@@ -16,7 +16,17 @@ async def get_project_issues(project_id:int,current_user: Annotated[User, Depend
     result = await db_get_project_issues(project_id,current_user)
     return result
 
-@router.post("/{project_id}/issues",response_model=IssueOut)
-async def get_project_issues(project_id:int,issue_args:CreateIssue,current_user: Annotated[User, Depends(get_current_user)]):
+@router.post("/{project_id}/issues",response_model=IssueOut,status_code=status.HTTP_201_CREATED)
+async def create_project_issue(project_id:int,issue_args:IssueIn,current_user: Annotated[User, Depends(get_current_user)]):
     issue = await db_create_issue(project_id,issue_args,current_user)
     return issue
+
+@router.put("/{project_id}/issues/{issue_id}",response_model=IssueOut)
+async def update_project_issue(project_id:int,issue_id:int,issue_args:IssueIn,current_user: Annotated[User, Depends(get_current_user)]):
+    issue = await db_update_issue(project_id,issue_id,issue_args,current_user)
+    return issue
+
+@router.delete("/{project_id}/issues/{issue_id}",status_code=status.HTTP_204_NO_CONTENT)
+async def delete_project_issue(project_id:int,issue_id:int,current_user: Annotated[User, Depends(get_current_user)]):
+    await db_delete_issue(project_id,issue_id,current_user)
+    return 
