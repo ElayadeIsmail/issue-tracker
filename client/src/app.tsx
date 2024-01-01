01fbb1e464658c250f341c9fd8594545158d6ea7
Elayade Ismail <elayadeismail@gmail.com>
2024-01-01 20:17:22 +0100

    Add Nunito Sans Font

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -3,7 +3,8 @@ import { MetaProvider } from '@solidjs/meta';
 import { Router } from '@solidjs/router';
 import { FileRoutes } from '@solidjs/start';
 import { Suspense } from 'solid-js';
-import './app.css';
+import './styles/app.css';
+import './styles/font.css';
 
 export default function App() {
 	return (
