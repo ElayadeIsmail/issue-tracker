ad1ee7b93308da1f051b420ceceaf30aaf9771cc
Elayade Ismail <elayadeismail@gmail.com>
2024-01-01 15:21:32 +0100

    Add Public Header

---
+++ client/src/components/BaseHeader.tsx
@@ -0,0 +1,21 @@
+import { A } from '@solidjs/router';
+import { BugIcon } from './icons';
+import { Button } from './ui/Button';
+
+export function BaseHeader() {
+	return (
+		<header class='container flex items-center justify-between h-16 border-b'>
+			<A href='/'>
+				<BugIcon />
+			</A>
+			<nav class='space-x-2'>
+				<A href='/login'>
+					<Button>Login</Button>
+				</A>
+				<A href='/register'>
+					<Button variant='secondary'>Register</Button>
+				</A>
+			</nav>
+		</header>
+	);
+}
